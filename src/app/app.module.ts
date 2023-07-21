b5907abdc8dc34165c574054fb8277f4bf7c43e9
Juan1Esteban <juanpismanrique@gmail.com>
2023-07-21 18:43:03 -0500

    Creacion directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { VacalesPipe } from './pipes/vacales.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { VacalesPipe } from './pipes/vacales.pipe';
     NavComponent,
     ReversePipe,
     TimeAgoPipe,
-    VacalesPipe
+    VacalesPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
